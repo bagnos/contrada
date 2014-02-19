a83c363f76a64f5aab33d67d3d0a23db4aae4d48
bagnos <simone.bagnolesi@gmail.com>
2014-02-19 10:47:06 +0100

    s

---
+++ ContradaCommon/src/it/contrada/dto/PosteSottoscrizioneDTO.java
@@ -0,0 +1,182 @@
+package it.contrada.dto;
+
+import java.io.Serializable;
+
+public class PosteSottoscrizioneDTO implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String riga1;
+	private String riga2;
+	private String riga3;
+	private String campo1;
+	private String campo2;
+	private String campo3;
+	private String campo4;
+	private String campo5;
+	private String campo6;
+	private String campo7;
+	private String campo8;
+	private String campo9;
+	private String campo10;
+	private String campo11;
+	private String campo12;
+	private String campo13;
+	private String cap;
+	private String citta;
+	private String prov;
+
+	public String getCap() {
+		return cap;
+	}
+
+	public void setCap(String cap) {
+		this.cap = cap;
+	}
+
+	public String getCitta() {
+		return citta;
+	}
+
+	public void setCitta(String citta) {
+		this.citta = citta;
+	}
+
+	public String getProv() {
+		return prov;
+	}
+
+	public void setProv(String prov) {
+		this.prov = prov;
+	}
+
+	public String getRiga1() {
+		return riga1;
+	}
+
+	public void setRiga1(String riga1) {
+		this.riga1 = riga1;
+	}
+
+	public String getRiga2() {
+		return riga2;
+	}
+
+	public void setRiga2(String riga2) {
+		this.riga2 = riga2;
+	}
+
+	public String getRiga3() {
+		return riga3;
+	}
+
+	public void setRiga3(String riga3) {
+		this.riga3 = riga3;
+	}
+
+	public String getCampo1() {
+		return campo1;
+	}
+
+	public void setCampo1(String campo1) {
+		this.campo1 = campo1;
+	}
+
+	public String getCampo2() {
+		return campo2;
+	}
+
+	public void setCampo2(String campo2) {
+		this.campo2 = campo2;
+	}
+
+	public String getCampo3() {
+		return campo3;
+	}
+
+	public void setCampo3(String campo3) {
+		this.campo3 = campo3;
+	}
+
+	public String getCampo4() {
+		return campo4;
+	}
+
+	public void setCampo4(String campo4) {
+		this.campo4 = campo4;
+	}
+
+	public String getCampo5() {
+		return campo5;
+	}
+
+	public void setCampo5(String campo5) {
+		this.campo5 = campo5;
+	}
+
+	public String getCampo6() {
+		return campo6;
+	}
+
+	public void setCampo6(String campo6) {
+		this.campo6 = campo6;
+	}
+
+	public String getCampo7() {
+		return campo7;
+	}
+
+	public void setCampo7(String campo7) {
+		this.campo7 = campo7;
+	}
+
+	public String getCampo8() {
+		return campo8;
+	}
+
+	public void setCampo8(String campo8) {
+		this.campo8 = campo8;
+	}
+
+	public String getCampo9() {
+		return campo9;
+	}
+
+	public void setCampo9(String campo9) {
+		this.campo9 = campo9;
+	}
+
+	public String getCampo10() {
+		return campo10;
+	}
+
+	public void setCampo10(String campo10) {
+		this.campo10 = campo10;
+	}
+
+	public String getCampo11() {
+		return campo11;
+	}
+
+	public void setCampo11(String campo11) {
+		this.campo11 = campo11;
+	}
+
+	public String getCampo12() {
+		return campo12;
+	}
+
+	public void setCampo12(String campo12) {
+		this.campo12 = campo12;
+	}
+
+	public String getCampo13() {
+		return campo13;
+	}
+
+	public void setCampo13(String campo13) {
+		this.campo13 = campo13;
+	}
+
+}
